31b8cb54091a11be34ff75b585c5270f77acfbc8
Dmitry <onexevs@gmail.com>
2023-09-04 21:05:36 +0300

    added print_title function

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def print_title():
+    title = "| Python simple calculator for evoacademy |"
+    title_len = len(title)
+    print("="*title_len)
+    print(title)
+    print("="*title_len)
+
+def main():
+    print_title()
+    
+
+
+
+if __name__ == "__main__":
+    main()
